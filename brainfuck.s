49c9576697d178cafe687372a2ad924bfffdb893
Ashma Zaheer <43542586+Zaheer-A@users.noreply.github.com>
2019-10-24 12:52:58 +0200

    Comments
    Added comments to the subroutines for better understanding.

--- brainfuck.s
+++ brainfuck.s
@@ -19,52 +19,52 @@ brainfuck:
 
 # *********************************************INSERT CODE **************************************************************************
 	movq $0, %rdx				#  %rdx as our data pointer and initializing 
-	movq $-1 , %rcx 				# %rcx is our counter to loop through the string in the file
+	movq $-1 , %rcx 			# %rcx is our counter to loop through the string in the file
 	
 nextChar:	
-	incq %rcx						# Increment the counter
+	incq %rcx				# Increment the counter
 	movb (%rsi, %rcx, 1), %al		# get the next character from the string into register			
 	cmpb $0, %al				# checking if the char (byte) is a zero byte (end of string)
-	jne	loop1					# if it is not a zero byte, then we jump to our loop subruotine
-	jmp endsub					# if it is a zero byte (end of string) we end our brainfuck interpreter.
+	jne	loop1				# if it is not a zero byte, then we jump to our loop subruotine
+	jmp endsub				# if it is a zero byte (end of string) we end our brainfuck interpreter.
 	
 	
  		
 loop1:	
-	plus:						# check if the character in %al is a plus operator
-		movb $'+', %r8			# moving the + char for compare operation
+	plus:					# check if the character in %al is a plus operator
+		movb $'+', %r8			# moving the '+' char for compare operation
 		cmpb %al, %r8
-		jne minus				# if it is not a + then go to the next check subroutine
+		jne minus			# if it is not a '+' then go to the next check subroutine
 
-		incb CELL(%rdx)			# increment the cell at position %rdx
+		incb CELL(%rdx)			# increment the value in the cell that is currently being pointed to by %rdx
 		jmp nextChar			# go back to nextChar to get the next character from the string
 
 	minus:	
-		movb $'-', %r8
-		cmpb %al, %r8
-		jne pointer_up
+		movb $'-', %r8			# moving the '-' char for compare operation
+		cmpb %al, %r8			
+		jne pointer_up			# if it is not a '-' then go to the next check subroutine
 
-		decb CELL(%rdx)	 
+		decb CELL(%rdx)			# decrement the value in the cell that is currently being pointed to by %rdx	 
 
-		jmp nextChar
+		jmp nextChar			# go back to nextChar to get the next character from the string.
 
 	pointer_up:
-		movb $'>', %r8
-		cmpb %al, %r8
-		jne pointer_down
+		movb $'>', %r8			# move the '>' char for compare operation
+		cmpb %al, %r8			
+		jne pointer_down		# if it is not a '>' then go to the next check subroutine
 
-		inc %rdx
+		inc %rdx			# increment the cell pointer to point to the next cell
 
-		jmp nextChar
+		jmp nextChar			# go back to nextChar to get the next character from the string.
 
 	pointer_down:
-		movb $'<', %r8
-		cmpb %al, %r8
-		jne loop_open
+		movb $'<', %r8			# move the '<' char for compare operation
+		cmpb %al, %r8			
+		jne loop_open			# if it is not a '<' then go to the next check subroutine
 
-		dec %rdx
+		dec %rdx			# decrement the cell pointer to point to the previous cell
 
-		jmp nextChar		
+		jmp nextChar			# go back to nextChar to get the next character from the string.
 
 	loop_open:
 		movb $'[', %r8
